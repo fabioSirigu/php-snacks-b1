07ef97660624460e51b3e7007b4c73df08db9768
naz1ne <fabio.sirigu.1995@gmail.com>
2022-11-28 12:10:33 +0100

    add snack1

---
+++ snack1/index.php
@@ -0,0 +1,17 @@
+<?php
+/* ## Snack 1
+
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+
+Olimpia Milano - Cantù | 55-60 */
+$calendario = [
+      'Milano - Varese / 55-60',
+      'Lucca - Cagliari / 65-83',
+      'Bergamo - Catania / 44-61',
+      'Torino - Genova / 70-60'
+];
+
+foreach ($calendario as $partita) {
+      echo $partita;
+      echo '<br>';
+}
