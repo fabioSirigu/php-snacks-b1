9a4c2b40180515e1b5c039ea3f6f71b2e7ca9f6e
naz1ne <fabio.sirigu.1995@gmail.com>
2022-11-28 12:22:33 +0100

    add array associativa

--- snack1/index.php
+++ snack1/index.php
@@ -5,13 +5,17 @@ Creiamo un array contenente le partite di basket di un’ipotetica tappa del cal
 
 Olimpia Milano - Cantù | 55-60 */
 $calendario = [
-      'Milano - Varese / 55-60',
-      'Lucca - Cagliari / 65-83',
-      'Bergamo - Catania / 44-61',
-      'Torino - Genova / 70-60'
+      ['Milano', 'Varese', 55, 60],
+      ['Lucca', 'Cagliari', 65, 83],
+      ['Bergamo', 'Catania', 44, 61],
+      ['Torino', 'Genova', 70, 60]
 ];
 
 foreach ($calendario as $partita) {
-      echo $partita;
-      echo '<br>';
+      foreach ($partita as $key => $results) {
+            echo $results . ' ';
+            if ($key == count($partita) - 1) {
+                  echo '<br>';
+            }
+      }
 }
